4d8561d651cecdf1cc684e13fd97e76887caaf63
Ekaterina Pirogova <2535545@bk.ru>
2018-11-15 09:42:12 -0800

    Add files via upload
    This class will create a complex encoding of nominal values in a way described in this paper https://arxiv.org/pdf/1601.01966.pdf

---
+++ ComplexEncoder.py
@@ -0,0 +1,66 @@
+class ComplexEncoder():
+
+    #returns mapings of category to value
+    def fit(self,column):
+        #count frequencies of each category
+        val_counts = column.value_counts() #it creates sorted from largest to smallest pandas series
+        #count number of categories
+        k = column.nunique()-1
+
+        #create a dictionary of j values for every group
+        #smallest values of i goes to the most frequent groups
+        j = {}
+        for i in range(0,column.nunique()):
+            j[column.value_counts().index[i]] = i #i=0 - largest group
+
+        #create dictionary with module R for every group
+        module_R = {}
+        for level in range(0,len(column.value_counts())):
+            z=0
+            for i in range(1,column.value_counts()[level]+1):
+                z+=i
+            module_R[column.value_counts().index[level]]=z/column.value_counts()[level]
+
+        #find all ties
+        ties = {}
+        for key, value in module_R.items():
+            try:
+                ties[value].append(key)
+            except KeyError:
+                ties[value] = [key]
+        ties_list = [l for l in ties.values() if len(l)>1]
+        not_ties = [l for l in ties.values() if len(l)==1]
+        #calculate phase for every ties level
+        phase = {}
+        for d in range(0,len(val_counts)):
+            m=0
+            #val_counts has all levels of category, for this reason levels d for ties and not ties are less than val counts
+            #for this reason we need to pass these levels of d, which do not have the level d
+            try:
+                for i in ties_list[d]:
+                    if m==0:
+                        phase[i]=0
+                    else:
+                        phase[i]=math.pi*j[i]/k
+                    m+=1
+            except:
+                pass
+            try:
+                for i in not_ties[d]:
+                    phase[i]=0
+            except:
+                pass
+        #calculate exponential form
+        self.exp = {}
+        for key, value in module_R.items():
+            self.exp[key] = value*np.exp(cmath.sqrt(-1)*phase[key])
+
+        return self
+
+    #map category to corresponding value
+    def transform(self, column):
+         #map exponential form instead of categorical for column
+        column=column.map(self.exp)
+        return column
+    def fit_transform(self, column):
+        return self.fit(column).transform(column)
